633a17eefc328162efccadea205ace9e4cb19998
Michael Macias <zaeleus@gmail.com>
2019-01-31 19:56:04 -0600

    Use noodles' gff record parser

--- src/lib.rs
+++ src/lib.rs
@@ -11,18 +11,10 @@ use std::io;
 use std::ops::Range;
 use std::path::Path;
 
-use csv::StringRecord;
 use interval_tree::IntervalTree;
 use log::info;
 use noodles::formats::bam::{Cigar, Flag, cigar};
 use noodles::formats::gff;
-
-const GFF_SEQ_NAME_INDEX: usize = 0;
-const GFF_TYPE_INDEX: usize = 2;
-const GFF_START_INDEX: usize = 3;
-const GFF_END_INDEX: usize = 4;
-const GFF_STRAND_INDEX: usize = 6;
-const GFF_ATTRS_INDEX: usize = 8;
 
 pub type Features = HashMap<String, IntervalTree<u64, Entry>>;
 
@@ -44,19 +36,28 @@ where
     info!("reading features");
 
     for result in reader.records() {
-        let record = result?;
+        let row = result?;
+        let record = gff::Record::new(row);
 
-        let ty = parse_type(&record)?;
+        let ty = record.feature().map_err(invalid_data)?;
 
         if ty != feature_type {
             continue;
         }
 
-        let seq_name = parse_seq_name(&record)?;
-        let start = parse_start(&record)?;
-        let end = parse_end(&record)?;
-        let strand = parse_strand(&record)?;
-        let id = parse_attrs_and_get(&record, feature_id)?;
+        let seq_name = record.seq_name().map_err(invalid_data)?;
+        let start = record.start().map_err(invalid_data)?;
+        let end = record.end().map_err(invalid_data)?;
+
+        let strand = record.strand().map_err(invalid_data).and_then(parse_strand)?;
+
+        let attributes = record.attributes().map_err(invalid_data)?;
+        let id = attributes.get(feature_id).ok_or_else(|| {
+            io::Error::new(
+                io::ErrorKind::InvalidData,
+                format!("missing attribute '{}'", feature_id),
+            )
+        })?;
 
         let tree = features.entry(seq_name.to_string()).or_default();
         tree.insert(start..end, Entry(id.to_string(), strand));
@@ -69,60 +70,12 @@ where
     Ok((features, names))
 }
 
-fn parse_type(record: &StringRecord) -> io::Result<&str> {
-    record.get(GFF_TYPE_INDEX).ok_or_else(|| {
-        io::Error::new(io::ErrorKind::InvalidData, "invalid type")
-    })
+fn invalid_data(e: gff::record::Error) -> io::Error {
+    io::Error::new(io::ErrorKind::InvalidData, e)
 }
 
-fn parse_seq_name(record: &StringRecord) -> io::Result<&str> {
-    record.get(GFF_SEQ_NAME_INDEX).ok_or_else(|| {
-        io::Error::new(io::ErrorKind::InvalidData, "invalid seq name")
-    })
-}
-
-fn parse_start(record: &StringRecord) -> io::Result<u64> {
-    record
-        .get(GFF_START_INDEX)
-        .and_then(|s| s.parse::<u64>().ok())
-        .map(|s| s - 1)
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "invalid start"))
-}
-
-fn parse_end(record: &StringRecord) -> io::Result<u64> {
-    record
-        .get(GFF_END_INDEX)
-        .and_then(|s| s.parse().ok())
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "invalid end"))
-}
-
-fn parse_strand(record: &StringRecord) -> io::Result<Strand> {
-    record
-        .get(GFF_STRAND_INDEX)
-        .and_then(|s| s.parse().ok())
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "invalid strand"))
-}
-
-fn parse_attrs_and_get<'a>(record: &'a StringRecord, key: &str) -> io::Result<&'a str> {
-    let attrs = record
-        .get(GFF_ATTRS_INDEX)
-        .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "invalid attrs"))?;
-
-    for attr in attrs.split(';').map(str::trim_left) {
-        if attr.starts_with(key) {
-            return attr.split('"').nth(1).ok_or_else(|| {
-                io::Error::new(
-                    io::ErrorKind::InvalidInput,
-                    format!("could not parse attribute '{}'", attr),
-                )
-            });
-        }
-    }
-
-    Err(io::Error::new(
-        io::ErrorKind::InvalidInput,
-        format!("missing attribute '{}'", key),
-    ))
+fn parse_strand(s: &str) -> io::Result<Strand> {
+    s.parse().map_err(|e| io::Error::new(io::ErrorKind::InvalidData, e))
 }
 
 pub struct CigarToIntervals<'a> {
@@ -159,71 +112,5 @@ impl<'a> Iterator for CigarToIntervals<'a> {
 
             self.start += len;
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use csv::StringRecord;
-
-    use crate::Strand;
-
-    use super::*;
-
-    fn build_record() -> StringRecord {
-        StringRecord::from(vec![
-           "chr1",
-           "HAVANA",
-           "gene",
-           "11869",
-           "14409",
-           ".",
-           "+",
-           ".",
-           r#"gene_id "ENSG00000223972.5"; gene_type "transcribed_unprocessed_pseudogene"; gene_name "DDX11L1"; level 2; havana_gene "OTTHUMG00000000961.2";"#
-        ])
-    }
-
-    #[test]
-    pub fn test_parse_type() {
-        let record = build_record();
-        assert_eq!(parse_type(&record).unwrap(), "gene");
-    }
-
-    #[test]
-    pub fn test_parse_seq_name() {
-        let record = build_record();
-        assert_eq!(parse_seq_name(&record).unwrap(), "chr1");
-    }
-
-    #[test]
-    pub fn test_parse_start() {
-        let record = build_record();
-        assert_eq!(parse_start(&record).unwrap(), 11868);
-    }
-
-    #[test]
-    pub fn test_parse_end() {
-        let record = build_record();
-        assert_eq!(parse_end(&record).unwrap(), 14409);
-    }
-
-    #[test]
-    pub fn test_parse_strand() {
-        let record = build_record();
-        assert_eq!(parse_strand(&record).unwrap(), Strand::Forward);
-    }
-
-    #[test]
-    fn test_parse_attrs_and_get() {
-        let record = build_record();
-
-        assert_eq!(parse_attrs_and_get(&record, "gene_id").unwrap(), "ENSG00000223972.5");
-        assert_eq!(parse_attrs_and_get(&record, "gene_type").unwrap(), "transcribed_unprocessed_pseudogene");
-        assert_eq!(parse_attrs_and_get(&record, "gene_name").unwrap(), "DDX11L1");
-        assert_eq!(parse_attrs_and_get(&record, "havana_gene").unwrap(), "OTTHUMG00000000961.2");
-
-        assert!(parse_attrs_and_get(&record, "level").is_err());
-        assert!(parse_attrs_and_get(&record, "dne").is_err());
     }
 }
