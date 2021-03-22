6aff89732c8ee107d8bb0427435fe7ad9b20b95c
Michael Macias <zaeleus@gmail.com>
2021-03-22 18:48:13 -0500

    tests: Add integration test for single-stranded, forward sample

---
+++ tests/quantify_single_end_forward_test.rs
@@ -0,0 +1,48 @@
+use std::{env, fs};
+
+use noodles_squab::{commands::quantify, count::Filter, StrandSpecificationOption};
+
+#[test]
+fn test_quantify_with_single_end_forward_sample() -> anyhow::Result<()> {
+    let feature_type = "exon";
+    let id = "gene_id";
+    let filter = Filter::new(10, false, false, false);
+    let strand_specification_option = StrandSpecificationOption::Auto;
+    let threads = 1;
+    let normalize = None;
+
+    let working_prefix = env::temp_dir();
+    fs::create_dir_all(&working_prefix)?;
+
+    let results_dst = working_prefix.join("out.txt");
+
+    quantify(
+        "tests/fixtures/sample.single-end.forward-strand.bam",
+        "tests/fixtures/annotations.gff3",
+        feature_type,
+        id,
+        filter,
+        strand_specification_option,
+        threads,
+        normalize,
+        &results_dst,
+    )?;
+
+    let actual = fs::read_to_string(results_dst)?;
+
+    let expected = "\
+sq0g0\t4
+sq1g0\t2
+sq2g0\t0
+sq2g1\t0
+__no_feature\t5
+__ambiguous\t1
+__too_low_aQual\t1
+__not_aligned\t0
+__alignment_not_unique\t1
+";
+
+    assert_eq!(actual, expected);
+
+    Ok(())
+}
