bbbbd7b984cdd3082c9fd1ed0df1a3b7686c55fe
Michael Macias <zaeleus@gmail.com>
2021-12-31 16:59:40 -0600

    Update to clap 3.0.0

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,33 @@ use mimalloc::MiMalloc;
 #[global_allocator]
 static GLOBAL: MiMalloc = MiMalloc;
 
-use clap::{crate_name, value_t, App, AppSettings, Arg, ArgMatches, SubCommand};
+use clap::{crate_name, App, AppSettings, Arg, ArgMatches};
 use git_testament::{git_testament, render_testament};
-use noodles_squab::{commands, count::Filter, normalization, StrandSpecificationOption};
+use noodles_squab::{commands, count::Filter};
 use tracing::{info, warn};
 
 git_testament!(TESTAMENT);
 
-fn match_args_from_env() -> clap::ArgMatches<'static> {
-    let quantify_cmd = SubCommand::with_name("quantify")
+fn match_args_from_env() -> clap::ArgMatches {
+    let quantify_cmd = App::new("quantify")
         .about("Gene expression quantification")
         .arg(
-            Arg::with_name("with-secondary-records")
+            Arg::new("with-secondary-records")
                 .long("with-secondary-records")
                 .help("Count secondary records (BAM flag 0x100)"),
         )
         .arg(
-            Arg::with_name("with-supplementary-records")
+            Arg::new("with-supplementary-records")
                 .long("with-supplementary-records")
                 .help("Count supplementary records (BAM flag 0x800)"),
         )
         .arg(
-            Arg::with_name("with-nonunique-records")
+            Arg::new("with-nonunique-records")
                 .long("with-nonunique-records")
                 .help("Count nonunique records (BAM data tag NH > 1)"),
         )
         .arg(
-            Arg::with_name("strand-specification")
+            Arg::new("strand-specification")
                 .long("strand-specification")
                 .value_name("str")
                 .help("Strand specification")
@@ -37,85 +37,85 @@ fn match_args_from_env() -> clap::ArgMatches<'static> {
                 .default_value("auto"),
         )
         .arg(
-            Arg::with_name("feature-type")
-                .short("t")
+            Arg::new("feature-type")
+                .short('t')
                 .long("feature-type")
                 .value_name("str")
                 .help("Feature type to count")
                 .default_value("exon"),
         )
         .arg(
-            Arg::with_name("id")
-                .short("i")
+            Arg::new("id")
+                .short('i')
                 .long("id")
                 .value_name("str")
                 .help("Feature attribute to use as the feature identity")
                 .default_value("gene_id"),
         )
         .arg(
-            Arg::with_name("min-mapping-quality")
+            Arg::new("min-mapping-quality")
                 .long("min-mapping-quality")
                 .value_name("u8")
                 .help("Minimum mapping quality to consider an alignment")
                 .default_value("10"),
         )
         .arg(
-            Arg::with_name("output")
-                .short("o")
+            Arg::new("output")
+                .short('o')
                 .long("output")
                 .value_name("file")
                 .help("Output destination for feature counts")
                 .required(true),
         )
         .arg(
-            Arg::with_name("annotations")
-                .short("a")
+            Arg::new("annotations")
+                .short('a')
                 .long("annotations")
                 .value_name("file")
                 .help("Input annotations file (GFF3)")
                 .required(true),
         )
         .arg(
-            Arg::with_name("threads")
+            Arg::new("threads")
                 .long("threads")
                 .value_name("uint")
                 .help("Force a specific number of threads"),
         )
         .arg(
-            Arg::with_name("bam")
+            Arg::new("bam")
                 .help("Input alignment file")
                 .required(true)
                 .index(1),
         );
 
-    let normalize_cmd = SubCommand::with_name("normalize")
+    let normalize_cmd = App::new("normalize")
         .about("Normalize counts")
         .arg(
-            Arg::with_name("feature-type")
-                .short("t")
+            Arg::new("feature-type")
+                .short('t')
                 .long("feature-type")
                 .value_name("str")
                 .help("Feature type to count")
                 .default_value("exon"),
         )
         .arg(
-            Arg::with_name("id")
-                .short("i")
+            Arg::new("id")
+                .short('i')
                 .long("id")
                 .value_name("str")
                 .help("Feature attribute to use as the feature identity")
                 .default_value("gene_id"),
         )
         .arg(
-            Arg::with_name("annotations")
-                .short("a")
+            Arg::new("annotations")
+                .short('a')
                 .long("annotations")
                 .value_name("file")
                 .help("Input annotations file (GFF3)")
                 .required(true),
         )
         .arg(
-            Arg::with_name("method")
+            Arg::new("method")
                 .long("method")
                 .value_name("str")
                 .help("Quantification normalization method")
@@ -123,7 +123,7 @@ fn match_args_from_env() -> clap::ArgMatches<'static> {
                 .default_value("tpm"),
         )
         .arg(
-            Arg::with_name("counts")
+            Arg::new("counts")
                 .help("Input counts file")
                 .required(true)
                 .index(1),
@@ -133,18 +133,18 @@ fn match_args_from_env() -> clap::ArgMatches<'static> {
         .version(render_testament!(TESTAMENT).as_str())
         .setting(AppSettings::SubcommandRequiredElseHelp)
         .arg(
-            Arg::with_name("verbose")
-                .short("v")
+            Arg::new("verbose")
+                .short('v')
                 .long("verbose")
                 .help("Use verbose logging")
-                .hidden(true),
+                .hide(true),
         )
         .subcommand(quantify_cmd)
         .subcommand(normalize_cmd)
         .get_matches()
 }
 
-fn quantify(matches: &ArgMatches<'_>) -> anyhow::Result<()> {
+fn quantify(matches: &ArgMatches) -> anyhow::Result<()> {
     let bam_src = matches.value_of("bam").unwrap();
     let annotations_src = matches.value_of("annotations").unwrap();
 
@@ -152,18 +152,21 @@ fn quantify(matches: &ArgMatches<'_>) -> anyhow::Result<()> {
 
     let feature_type = matches.value_of("feature-type").unwrap();
     let id = matches.value_of("id").unwrap();
-    let min_mapping_quality =
-        value_t!(matches, "min-mapping-quality", u8).unwrap_or_else(|e| e.exit());
+    let min_mapping_quality = matches
+        .value_of_t("min-mapping-quality")
+        .unwrap_or_else(|e| e.exit());
 
     let with_secondary_records = matches.is_present("with-secondary-records");
     let with_supplementary_records = matches.is_present("with-supplementary-records");
     let with_nonunique_records = matches.is_present("with-nonunique-records");
 
-    let threads = value_t!(matches, "threads", usize).unwrap_or_else(|_| num_cpus::get());
+    let threads = matches
+        .value_of_t("threads")
+        .unwrap_or_else(|_| num_cpus::get());
 
-    let strand_specification_option =
-        value_t!(matches, "strand-specification", StrandSpecificationOption)
-            .unwrap_or_else(|e| e.exit());
+    let strand_specification_option = matches
+        .value_of_t("strand-specification")
+        .unwrap_or_else(|e| e.exit());
 
     let filter = Filter::new(
         min_mapping_quality,
@@ -190,14 +193,14 @@ fn quantify(matches: &ArgMatches<'_>) -> anyhow::Result<()> {
     ))
 }
 
-fn normalize(matches: &ArgMatches<'_>) -> anyhow::Result<()> {
+fn normalize(matches: &ArgMatches) -> anyhow::Result<()> {
     let counts_src = matches.value_of("counts").unwrap();
     let annotations_src = matches.value_of("annotations").unwrap();
 
     let feature_type = matches.value_of("feature-type").unwrap();
     let id = matches.value_of("id").unwrap();
 
-    let method = value_t!(matches, "method", normalization::Method).unwrap_or_else(|e| e.exit());
+    let method = matches.value_of_t("method").unwrap_or_else(|e| e.exit());
 
     commands::normalize(counts_src, annotations_src, feature_type, id, method)
 }
