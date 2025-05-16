49c8830cc7a8ee67f228830ca4fcbf785d29b51a
Michael Macias <zaeleus@gmail.com>
2025-05-16 16:19:59 -0500

    normalization/tests: Add feature builder

--- src/normalization.rs
+++ src/normalization.rs
@@ -77,54 +77,53 @@ fn merge_features(features: &[Feature]) -> Vec<Feature> {
 
 #[cfg(test)]
 mod tests {
-    use crate::Feature;
-
-    use noodles::{core::Position, gff};
+    use noodles::{core::Position, gff::feature::record::Strand};
 
     use super::*;
+    use crate::Feature;
 
-    #[test]
-    fn test_sum_nonoverlapping_feature_lengths()
-    -> Result<(), noodles::core::position::TryFromIntError> {
-        let strand = gff::feature::record::Strand::Forward;
+    const STRAND: Strand = Strand::Forward;
 
-        let features = [
-            Feature::new(0, Position::try_from(2)?, Position::try_from(5)?, strand),
-            Feature::new(0, Position::try_from(3)?, Position::try_from(4)?, strand),
-            Feature::new(0, Position::try_from(5)?, Position::try_from(7)?, strand),
-            Feature::new(0, Position::try_from(9)?, Position::try_from(12)?, strand),
-            Feature::new(0, Position::try_from(10)?, Position::try_from(15)?, strand),
-            Feature::new(0, Position::try_from(16)?, Position::try_from(21)?, strand),
-        ];
+    macro_rules! position {
+        ($n:literal) => {
+            const { Position::new($n).unwrap() }
+        };
+    }
 
-        let len = sum_nonoverlapping_feature_lengths(&features);
-        assert_eq!(len, 19);
+    fn build_features() -> [Feature; 6] {
+        //          111111111122222222222
+        // 123456789012345678901234567890
+        //  [--]   [--]   [----]
+        //   [][-]  [----]
 
-        Ok(())
+        [
+            Feature::new(0, position!(2), position!(5), STRAND),
+            Feature::new(0, position!(3), position!(4), STRAND),
+            Feature::new(0, position!(5), position!(7), STRAND),
+            Feature::new(0, position!(9), position!(12), STRAND),
+            Feature::new(0, position!(10), position!(15), STRAND),
+            Feature::new(0, position!(16), position!(21), STRAND),
+        ]
     }
 
     #[test]
-    fn test_merge_features() -> Result<(), noodles::core::position::TryFromIntError> {
-        let strand = gff::feature::record::Strand::Forward;
+    fn test_sum_nonoverlapping_feature_lengths() {
+        let features = build_features();
+        let len = sum_nonoverlapping_feature_lengths(&features);
+        assert_eq!(len, 19);
+    }
 
-        let features = [
-            Feature::new(0, Position::try_from(2)?, Position::try_from(5)?, strand),
-            Feature::new(0, Position::try_from(3)?, Position::try_from(4)?, strand),
-            Feature::new(0, Position::try_from(5)?, Position::try_from(7)?, strand),
-            Feature::new(0, Position::try_from(9)?, Position::try_from(12)?, strand),
-            Feature::new(0, Position::try_from(10)?, Position::try_from(15)?, strand),
-            Feature::new(0, Position::try_from(16)?, Position::try_from(21)?, strand),
-        ];
-
+    #[test]
+    fn test_merge_features() {
+        let features = build_features();
         let actual = merge_features(&features);
+
         let expected = [
-            Feature::new(0, Position::try_from(2)?, Position::try_from(7)?, strand),
-            Feature::new(0, Position::try_from(9)?, Position::try_from(15)?, strand),
-            Feature::new(0, Position::try_from(16)?, Position::try_from(21)?, strand),
+            Feature::new(0, position!(2), position!(7), STRAND),
+            Feature::new(0, position!(9), position!(15), STRAND),
+            Feature::new(0, position!(16), position!(21), STRAND),
         ];
 
         assert_eq!(actual, expected);
-
-        Ok(())
     }
 }
